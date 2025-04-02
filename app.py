ad6ae7f4798f8858c7c5f780476dfdf0d1319bbd
IT-BEST-Valencia <122632877+Parri0627@users.noreply.github.com>
2025-04-02 18:57:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, request, redirect
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    email = request.headers.get("X-MS-CLIENT-PRINCIPAL-NAME")
 
-# Para Azure
+    if not email:
+        return redirect("/.auth/login/google")
+
+    if not email.endswith("@bestvalencia.com"):
+        return "Acceso denegado: solo permitido para @bestvalencia.com", 403
+
+    return f"<h1>Bienvenido, {email}</h1>"
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host="0.0.0.0", port=8000)
+
