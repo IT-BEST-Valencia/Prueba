afe38c4a46c720b788174da7685762726efcd8c4
IT-BEST-Valencia <122632877+Parri0627@users.noreply.github.com>
2025-04-02 19:17:37 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,16 +6,14 @@ app = Flask(__name__)
 def home():
     email = request.headers.get("X-MS-CLIENT-PRINCIPAL-NAME")
 
-    print( "<pre>" + "\n".join(f"{k}: {v}" for k, v in headers.items()) + "</pre>")
-
     if not email:
         return redirect("/.auth/login/google")
 
     if not email.endswith("@bestvalencia.com"):
-        return "Acceso denegado: solo permitido para @bestvalencia.com", 403
+        return "<pre>" + "\n".join(f"{k}: {v}" for k, v in headers.items()) + "</pre>", 403
     
     headers = dict(request.headers)
-    
+    print( "<pre>" + "\n".join(f"{k}: {v}" for k, v in headers.items()) + "</pre>")
 
     return f"<h1>Bienvenido, {email}</h1>"
 
