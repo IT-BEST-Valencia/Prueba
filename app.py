89d7d313860e0cff13b642033ecf3bffc7bab680
IT-BEST-Valencia <122632877+Parri0627@users.noreply.github.com>
2025-04-04 16:09:53 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    email = request.headers.get("X-MS-CLIENT-PRINCIPAL-NAME")
+
+    if not email:
+        return redirect("/.auth/login/google")  # Redirección por si alguien llega sin sesión
+
+    return render_template("index.html", email=email)
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host="0.0.0.0", port=8000)
