662367960afcd5a07694a3a3e353c332f62482b9
IT-BEST-Valencia <122632877+Parri0627@users.noreply.github.com>
2025-04-04 12:55:51 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,9 @@
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    email = request.headers.get("X-MS-CLIENT-PRINCIPAL-NAME")
-
-    if not email:
-        return "No autenticado", 401
-
-    if not email.strip().lower().endswith("@bestvalencia.org"):
-        return "Acceso denegado", 403
-
-    return f"<h1>Bienvenido, {email}. La aplicación está funcionando y deberías solo estar viendo esto con una cuenta bestie.</h1>"
-
+    return render_template("index.html")
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
